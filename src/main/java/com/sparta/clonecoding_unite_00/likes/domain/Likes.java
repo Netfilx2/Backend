5c42771a1490554c3e62e7b08a242369779980ec
ryujiwoo184 <rty015@naver.com>
2022-08-23 21:55:52 +0900

    1. 영화, tvshow 상세페이지 좋아요 완료 2. 메인페이지 영화,tvshow 조회시 좋아요 확인 완료 3. 프로필 기능 완료

---
+++ src/main/java/com/sparta/clonecoding_unite_00/likes/domain/Likes.java
@@ -0,0 +1,34 @@
+package com.sparta.clonecoding_unite_00.likes.domain;
+
+import com.sparta.clonecoding_unite_00.member.doamin.Member;
+import com.sparta.clonecoding_unite_00.movie.domain.Video;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @JoinColumn(name = "memberId",nullable = false)
+    @ManyToOne (fetch = FetchType.LAZY)
+    private Member member;
+
+//    @Column(nullable = true)
+//    private Kakao kakaoID;
+
+    @JoinColumn (nullable = false)
+    @ManyToOne (fetch = FetchType.LAZY)
+    private Video video;
+
+    public Likes(Member member,Video video){
+        this.member = member;
+        this.video = video;
+    }
+}
