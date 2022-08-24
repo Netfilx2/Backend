c373ab8b3cf4b8ea576a6561be39dda3764c4855
pmjn1025 <pmjn1025@naver.com>
2022-08-25 00:59:09 +0900

    찜하기, 페이징, 영화검색, 카카오 로그인 프런트 확인 후 연동하기 다시 커밋

--- src/main/java/com/sparta/clonecoding_unite_00/kakaologin/dto/KakaoUserInfoDto.java
+++ src/main/java/com/sparta/clonecoding_unite_00/kakaologin/dto/KakaoUserInfoDto.java
@@ -1,17 +1,20 @@
-package com.sparta.clonecoding_unite_00.kakao;
+package com.sparta.clonecoding_unite_00.kakaologin.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class KakaoUserInfoDto {
 
-    private Long id;
+    //private Long id;
+    private String email;
     private String nickname;
-    private String loginId;
+
 
 }
